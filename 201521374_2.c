4cd260a2807f4921c366861005e0d4046c48b83e
River Jeong <ykjeong1223@naver.com>
2022-12-31 21:59:56 +0900

    WordListProgram

--- 201521374_2.c
+++ 201521374_2.c
@@ -1,41 +1,38 @@
-#include <stdio.h>  // printf,scanf사용을 위한 header
-void pri(int N) {   // 소수판별 및 출력 함수 정의
-        int i, j;   // for문에 사용할 변수
-        for (i = 2; i <= N; i++) {  // i를 2부터 N까지 1씩 증가시키며 반복 수행
-            for (j = 2; j <= i - 1; j++) {  // j를 2부터 i-1까지 1씩 증가시키며 반복 수행
-                        if (i%j == 0) { // 소수가 아닌(나누어떨어짐) 수일 경우
-                            break;  // 루프 탈출
-                        }
-                }
-                if (i == j) {   // 소수라면
-                        printf("%d ", i);   // 소수 출력
-                }
-        }
-        printf("\n");   // 가독성을 위함
-}
-int main() {    // main 시작
-    int num=0;  // 입력받을 변수
-    char o;     // 프로그램 계속실행or종료를 위한 변수
-    while(1){   // 반복문 시작
-        printf("2이상의 정수를 입력하세요 : "); // 사용자로부터 값을 입력받기위한 지시사항
-        scanf("%d", &num);  // 변수 num에 int로 저장
-        pri(num);   // pri함수 사용
-        getchar();  // c에서는 enter도 입력문자로 인식하므로 잘못된 문구출력방지
-        printf("프로그램을 종료하시겠습니까? (y/n)\n"); // 프로그램 종료 or 실행 여부 지시사항
-		scanf("%c", &o);    // char로 o에 저장
-		if (o == 'n' || o == 'N')   // n or N일경우
-		{
-			continue;   // 프로그램 다시 실행
-		}
-		else if(o == 'y' || o == 'Y')   // y or Y일경우
-		{
-			break;  // 루프 탈출
-		}
-		else    // 이외의 값을 받은경우
-		{
-			printf("잘못입력하였습니다. 다시 입력해주세요\n");  // 예외처리
-			continue;   // 다시 입력받음
-		}
+#define _CRT_SECURE_NO_WARNINGS    // strtok 보안 경고로 인한 컴파일 에러 방지
+#include <stdio.h>      // 표준 라이브러리함수 헤더
+#include <string.h>    // strtok 함수가 선언된 헤더 파일
+#include <stdlib.h>     // 헤더
+
+int main()  // main함수
+
+{
+
+    char s1[100];    // 크기가 30인 char형 배열을 선언하고 문자열 할당
+    char *p = (char *)malloc(strlen(s1) + 1);    // 문자열의 길이를 계산하여 크기에 맞게 기억공간을 동적할당
+    char* sArr[10] = { NULL, };  // 크기가 10인 문자열 포인터 배열을 선언하고 NULL로 초기화
+    
+    printf("문자열 입력: ");    // 사용자로부터 문자열 입력받기
+    gets(s1);   // s1에 저장
+
+    int i = 0;                     // 문자열 포인터 배열의 인덱스로 사용할 변수
+    char* ptr = strtok(s1, " .,");   // 공백 문자열을 기준으로 문자열을 자름
+
+    while (ptr != NULL)            // 자른 문자열이 나오지 않을 때까지 반복
+    {
+        sArr[i] = ptr;             // 문자열을 자른 뒤 메모리 주소를 문자열 포인터 배열에 저장
+        i++;                       // 인덱스 증가
+        ptr = strtok(NULL, " .,");   // 다음 문자열을 잘라서 포인터를 반환
     }
-        return 0;   // 반환값
+
+    printf("***** 단어리스트 *****\n"); // 잘라서 보관한 단어들 리스트 출력
+
+for (int i = 0; i < 10; i++)    // 반복문으로 인덱스 탐색
+    {
+        if (sArr[i] != NULL)           // 문자열 포인터 배열의 요소가 NULL이 아닐 때만
+            printf("%s\n", sArr[i]);   // 문자열 포인터 배열에 인덱스로 접근하여 각 문자열 출력
+    }
+
+    free(p);    // 동적 메모리 해제
+    return 0;   // 리턴값
+
 }
